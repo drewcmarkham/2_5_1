387b7020b69465cbc5bd231aed8aaa4ddb3622a7
drewcmarkham <drew.markham@icloud.com>
2024-12-03 09:24:09 -0800

    addPoints, getScore, getGuess

--- Player.java
+++ Player.java
@@ -5,28 +5,23 @@ public class Player {
     Scanner sc = new Scanner(System.in);
     private int player1Score = 0;
     private int player2Score = 0;
-
-    public Player() {
-        
-    }
-
+    private String guess = "";
+    
     public void addPoints(int player, int points) {
         if (player == 1) {
             player1Score += points;
         }
-        else if (player == 2) {
-            player2Score += points;
-        }
+        player2Score += points;
+    
     }
 
     public int getScore(int player) {
         if (player == 1) {
             return player1Score;
         }
-        else if (player == 2) {
-            return player2Score;
-        }
+        return player2Score;
     }
+
     public String getGuess() {
         System.out.println("Input your guess:");
         String getGuess = sc.nextLine();
